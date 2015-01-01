c644a18ee3fb4f736ab2be22952d5a233d44edf2
Michał Czardybon <mczard@poczta.onet.pl>
2015-01-01 23:14:34 +0100

    Re #6: Implemented protection against second instance.

--- Program.cs
+++ Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Security.Principal;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -14,9 +16,35 @@ namespace Herring
         [STAThread]
         static void Main()
         {
+            Mutex m = null;
+            if (!CheckUniqueInstance(out m))
+            {
+                if (m != null)
+                    GC.KeepAlive(m);
+                return;
+            }
+
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new MainForm());
+        }
+
+        private static bool CheckUniqueInstance(out Mutex m)
+        {
+            string mutexName = "Herring-" + WindowsIdentity.GetCurrent().User.ToString();
+
+            // check if application instance is already executed
+            bool createdNew;
+            m = new Mutex(true, mutexName, out createdNew);
+
+            if (!createdNew)
+            {
+                MessageBox.Show(
+                    "Only one instance of Herring Activity Tracker is allowed at a time.",
+                    "Herring Activity Tracker");
+                return false;
+            }
+            return true;
         }
     }
 }
