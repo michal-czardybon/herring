9d394022bd40dea489f6cf0422e340e2e76e2994
michal-czardybon <mczard@poczta.onet.pl>
2016-06-05 23:01:24 +0200

    Added reporting by days of week.

--- ReportForm.Designer.cs
+++ ReportForm.Designer.cs
@@ -29,16 +29,15 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.datePickerFrom = new System.Windows.Forms.DateTimePicker();
-            this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
-            this.datePickerTo = new System.Windows.Forms.DateTimePicker();
             this.calculateButton = new System.Windows.Forms.Button();
+            this.datePickerTo = new System.Windows.Forms.DateTimePicker();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.datePickerFrom = new System.Windows.Forms.DateTimePicker();
             this.chartBox1 = new System.Windows.Forms.PictureBox();
-            this.chartBox2 = new System.Windows.Forms.PictureBox();
+            this.reportText = new System.Windows.Forms.TextBox();
             this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chartBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chartBox2)).BeginInit();
             this.SuspendLayout();
             // 
             // panel1
@@ -54,33 +53,15 @@
             this.panel1.Size = new System.Drawing.Size(773, 58);
             this.panel1.TabIndex = 2;
             // 
-            // datePickerFrom
+            // calculateButton
             // 
-            this.datePickerFrom.CustomFormat = "dddd, dd MMMM yyyy";
-            this.datePickerFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.datePickerFrom.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
-            this.datePickerFrom.Location = new System.Drawing.Point(42, 3);
-            this.datePickerFrom.Name = "datePickerFrom";
-            this.datePickerFrom.Size = new System.Drawing.Size(257, 22);
-            this.datePickerFrom.TabIndex = 0;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(3, 9);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(33, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "From:";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(3, 35);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(23, 13);
-            this.label2.TabIndex = 2;
-            this.label2.Text = "To:";
+            this.calculateButton.Location = new System.Drawing.Point(306, 3);
+            this.calculateButton.Name = "calculateButton";
+            this.calculateButton.Size = new System.Drawing.Size(147, 50);
+            this.calculateButton.TabIndex = 4;
+            this.calculateButton.Text = "Calculate";
+            this.calculateButton.UseVisualStyleBackColor = true;
+            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
             // 
             // datePickerTo
             // 
@@ -92,15 +73,33 @@
             this.datePickerTo.Size = new System.Drawing.Size(257, 22);
             this.datePickerTo.TabIndex = 3;
             // 
-            // calculateButton
+            // label2
             // 
-            this.calculateButton.Location = new System.Drawing.Point(306, 3);
-            this.calculateButton.Name = "calculateButton";
-            this.calculateButton.Size = new System.Drawing.Size(147, 50);
-            this.calculateButton.TabIndex = 4;
-            this.calculateButton.Text = "Calculate";
-            this.calculateButton.UseVisualStyleBackColor = true;
-            this.calculateButton.Click += new System.EventHandler(this.calculateButton_Click);
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(3, 35);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(23, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "To:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(3, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(33, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "From:";
+            // 
+            // datePickerFrom
+            // 
+            this.datePickerFrom.CustomFormat = "dddd, dd MMMM yyyy";
+            this.datePickerFrom.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.datePickerFrom.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.datePickerFrom.Location = new System.Drawing.Point(42, 3);
+            this.datePickerFrom.Name = "datePickerFrom";
+            this.datePickerFrom.Size = new System.Drawing.Size(257, 22);
+            this.datePickerFrom.TabIndex = 0;
             // 
             // chartBox1
             // 
@@ -113,23 +112,22 @@
             this.chartBox1.TabIndex = 3;
             this.chartBox1.TabStop = false;
             // 
-            // chartBox2
+            // reportText
             // 
-            this.chartBox2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
-            this.chartBox2.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
-            this.chartBox2.Dock = System.Windows.Forms.DockStyle.Top;
-            this.chartBox2.Location = new System.Drawing.Point(0, 216);
-            this.chartBox2.Name = "chartBox2";
-            this.chartBox2.Size = new System.Drawing.Size(773, 158);
-            this.chartBox2.TabIndex = 4;
-            this.chartBox2.TabStop = false;
+            this.reportText.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.reportText.Font = new System.Drawing.Font("Courier New", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.reportText.Location = new System.Drawing.Point(0, 216);
+            this.reportText.Multiline = true;
+            this.reportText.Name = "reportText";
+            this.reportText.Size = new System.Drawing.Size(773, 162);
+            this.reportText.TabIndex = 4;
             // 
             // ReportForm
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(773, 378);
-            this.Controls.Add(this.chartBox2);
+            this.Controls.Add(this.reportText);
             this.Controls.Add(this.chartBox1);
             this.Controls.Add(this.panel1);
             this.Name = "ReportForm";
@@ -137,8 +135,8 @@
             this.panel1.ResumeLayout(false);
             this.panel1.PerformLayout();
             ((System.ComponentModel.ISupportInitialize)(this.chartBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.chartBox2)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
@@ -151,6 +149,6 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.PictureBox chartBox1;
-        private System.Windows.Forms.PictureBox chartBox2;
+        private System.Windows.Forms.TextBox reportText;
     }
 }
--- ReportForm.cs
+++ ReportForm.cs
@@ -32,17 +32,26 @@ namespace Herring
 
             double[] shares = new double[24 * 12];   // every 5 minutes
 
+            double[] weekly_nom = new double[7];
+            double[] weekly_den = new double[7];
+
             int count = 0;
             for (DateTime d = dateFrom; d <= dateTo; d = d.AddDays(1))
             {
                 count++;
 
+                double dailyTotal = 0;
                 List<ActivitySummary> summaries = Persistence.Load(getApp, d);
                 foreach (var s in summaries)
                 {
                     int k = (s.TimePoint.Hour * 60 + s.TimePoint.Minute) / 5;
                     shares[k] += s.TotalShare / 100.0;
+                    dailyTotal += s.TotalShare / 100.0 * Parameters.LogTimeUnit / 3600.0; // count hours
                 }
+
+                int q = ((int)d.DayOfWeek + 6) % 7;
+                weekly_nom[q] += dailyTotal;
+                weekly_den[q] += 1.0;
             }
 
             bitmap.Dispose();
@@ -55,7 +64,7 @@ namespace Herring
             {
                 double value = shares[i] / count;
                 int x1 = i * BAR_WIDTH;
-                int h  = (int)(BAR_HEIGHT * value);
+                int h = (int)(BAR_HEIGHT * value);
                 int y1 = BAR_HEIGHT - h + TOP_MARGIN;
 
                 g.FillRectangle(Brushes.Blue, x1, y1, BAR_WIDTH, h);
@@ -63,6 +72,19 @@ namespace Herring
             g.Dispose();
 
             chartBox1.Image = bitmap;
+
+            // Text
+            reportText.Lines =
+                new string[]
+                {
+                    String.Format("Monday:    {0:F2}", weekly_nom[0] / weekly_den[0]),
+                    String.Format("Tuesday:   {0:F2}", weekly_nom[1] / weekly_den[1]),
+                    String.Format("Wednesday: {0:F2}", weekly_nom[2] / weekly_den[2]),
+                    String.Format("Thursday:  {0:F2}", weekly_nom[3] / weekly_den[3]),
+                    String.Format("Friday:    {0:F2}", weekly_nom[4] / weekly_den[4]),
+                    String.Format("Saturaday: {0:F2}", weekly_nom[5] / weekly_den[5]),
+                    String.Format("Sunday:    {0:F2}", weekly_nom[6] / weekly_den[6]),
+                };
         }
     }
 }
